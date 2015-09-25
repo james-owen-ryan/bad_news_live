56c4c661ad6352be33aff4567fff0549f8382e8f
James Ryan <joryan@ucsc.edu>
2015-09-25 12:32:26 -0700

    Removed extraneous whitespace

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 
 
-app = Flask(__name__, static_url_path='/gameglobs/static')
+app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/wizard')
